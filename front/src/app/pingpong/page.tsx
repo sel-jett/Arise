e4da429df663ffd98aebf5fbda246e2a686509ed
olamrabti <oussamalamrabtii@gmail.com>
2025-04-13 20:45:45 +0100

    paddles now are moving

--- front/src/app/pingpong/page.tsx
+++ front/src/app/pingpong/page.tsx
@@ -1,46 +1,52 @@
 'use client';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect , useState} from 'react';
 
 
 const PingPong: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [leftPaddleY, setLeftPaddleY] = useState(250); 
+    const [RightPaddleY, setRightPaddleY] = useState(250);
+    const speed = 20;
 
     useEffect(() => {
-      const canvas = canvasRef.current;
-      if (!canvas) return;
-      const ctx = canvas.getContext('2d');
-      if (!ctx) return;
-  
-      const paddleWidth = 10;
-      const paddleHeight = 100;
-      const ballRadius = 8;
-      const paddleLeftX = 20;
-      const paddleRightX = canvas.width - 30;
-      const paddleLeftY = canvas.height / 2 - paddleHeight / 2;
-      const paddleRightY = canvas.height / 2 - paddleHeight / 2;
-      const ballX = canvas.width / 2;
-      const ballY = canvas.height / 2;
-  
+        const handleKeyDown = (e: KeyboardEvent) => {
+            console.log(`Key pressed: ${e.key}`);
+            if (e.key === 'ArrowUp')
+                setRightPaddleY((curr) => Math.max(curr - speed, 0))     
+            if (e.key === 'ArrowDown')
+                setRightPaddleY((curr) => Math.min(curr + speed, 520))
+            if (e.key === 'w')
+                setLeftPaddleY((curr) => Math.max(curr - speed, 0))     
+            if (e.key === 's')
+                setLeftPaddleY((curr) => Math.min(curr + speed, 520))
+            // console.log('right paddle y :', RightPaddleY);
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+          };
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
 
-      ctx.fillStyle = 'black';
-      ctx.fillRect(paddleLeftX, paddleLeftY, paddleWidth, paddleHeight);
-      ctx.fillRect(paddleRightX, paddleRightY, paddleWidth, paddleHeight); 
-  
-      ctx.beginPath();
-      ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
-      ctx.fillStyle = 'black';
-      ctx.fill();
-      ctx.closePath();
-  
-    }, []);
+    },[]);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+      
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      
+        ctx.fillStyle = 'black';
+        ctx.fillRect(20, leftPaddleY, 10, 80);
+      
+        ctx.fillRect(970, RightPaddleY, 10, 80);
+      }, [leftPaddleY, RightPaddleY]);
+      
+
 
     return(
         <>
         <div className="flex justify-center items-center h-screen bg-black">
-            <canvas ref={canvasRef} width={1000} height={600} className='bg-white'/> 
+            <canvas ref={canvasRef} width={1000} height={600} className='bg-white'/>
         </div>
         </>
     );
